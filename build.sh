04180c8e6b9605497719acd6526eea24fbc1550d
raf13lol <79861112+raf13lol@users.noreply.github.com>
2026-01-04 11:13:39 +0000

    1.3.2

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ dotnet build /p:BPE5=1
 cp bin/Debug/netstandard2.1/com.rhythmdr.oldcharacterreplacer.dll bin/Debug/netstandard2.1/com.rhythmdr.bpe5oldcharacterreplacer.dll
 echo "Building BPE6 build..."
 dotnet build
+echo "Zipping files..."
+rm -rf com.rhythmdr.oldcharacterreplacer.zip
+rm -rf com.rhythmdr.bpe5oldcharacterreplacer.zip
+zip -r com.rhythmdr.oldcharacterreplacer.zip ocrData bin/Debug/netstandard2.1/com.rhythmdr.oldcharacterreplacer.dll
+zip -r com.rhythmdr.bpe5oldcharacterreplacer.zip ocrData bin/Debug/netstandard2.1/com.rhythmdr.bpe5oldcharacterreplacer.dll
